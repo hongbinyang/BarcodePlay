0f8b48aa076efcc046a018ef2e23a513959e1beb
Hongbin Yang <hyang@expedia.com>
2015-04-27 11:41:04 -0700

    init check-in

---
+++ BarcodePlay/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BarcodePlay
+//
+//  Created by Hongbin Yang on 4/27/15.
+//  Copyright (c) 2015 Hongbin Yang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BarcodePlay/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BarcodePlay
+//
+//  Created by Hongbin Yang on 4/27/15.
+//  Copyright (c) 2015 Hongbin Yang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
