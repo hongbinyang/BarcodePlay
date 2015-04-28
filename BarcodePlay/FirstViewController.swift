856a38e6b046a95a0e1e91a6edd02128c8b70a99
Hongbin Yang <hyang@expedia.com>
2015-04-28 10:37:47 -0700

    Generate QR code

--- BarcodePlay/FirstViewController.swift
+++ BarcodePlay/FirstViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import AVFoundation
+import RSBarcodes
 
 class FirstViewController: UIViewController {
+
+    @IBOutlet weak var subjectHolder: UILabel!
+    
+    @IBOutlet weak var barView: UIImageView!
+
+    @IBOutlet weak var codeHolder: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        generateCode()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,24 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func toGenerate(sender: AnyObject) {
+        generateCode()
+    }
 
+    func generateCode () {
+        
+        let uuid = NSUUID().UUIDString
+
+        self.navigationItem.title = uuid
+        codeHolder.text = uuid
+        
+        let gen = RSUnifiedCodeGenerator.shared
+        gen.fillColor = UIColor.greenColor()
+        gen.strokeColor = UIColor.grayColor()
+        var image: UIImage? = gen.generateCode(uuid, machineReadableCodeObjectType: AVMetadataObjectTypeQRCode)
+        if let i = image {
+            self.barView.image = RSAbstractCodeGenerator.resizeImage(i, scale: 5.0)
+        }
+    }
 }
 
